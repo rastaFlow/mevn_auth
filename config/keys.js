24a5e455475cd55a21cc7379c215311cdea215f1
rastaFLOW <yohanaatelo22@gmail.com>
2020-02-28 15:32:59 +0200

    depolyment is ready appleaction

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,11 @@
-module.exports ={
-  mongoURI: "mongodb://localhost:27017/node_auth",
-  secret: "yoursecret"
+if(process.env.NODE_ENV === "production"){
+  module.exports ={
+    mongoURI: "mongodb+srv://node_auth:nodeauth123456789@nodeauth-dtmv7.mongodb.net/test?retryWrites=true&w=majority",
+    secret: "yoursecret"
+  };
+}else{
+  module.exports ={
+    mongoURI: "mongodb://localhost:27017/node_auth",
+    secret: "yoursecret"
+  };
 }
